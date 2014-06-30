4e1b007ef2226bf0e89a8a157a582a3e6069d4d6
CanisMajorisLT <frankaslt@yahoo.com>
2014-06-30 23:14:08 +0300

    *Remade so that all parsing (besides for keywords of ad) is done through rss feed. *Added try/except to some elements, because they are not in all ads (optional when making ad) *Cleaned up some code in regards to PEP8

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-__author__ = 'vyt'
+_author__ = 'vyt'
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
@@ -9,86 +9,108 @@ conn = sqlite3.connect("Database.db")
 
 # Takes furthest page link as argument and does magic
 def update_Ad_table_0(link):
+    rss_link = "http://www.cvonline.lt/darbo-skelbimai/visi?sort=tp_id&dir=desc&type=rss&page=0"
 
     # Makes a list of all the Url's that are currently in DB and of all active (marked as such) Url's in DB
-    listof_LinksInDB, listof_AllActiveUrlsInDB = make_listoflinkinDB()
+    list_of_links_in_DB, list_of_all_active_urls_in_DB = make_listoflinkinDB()
 
     page = requests.get(link)
     soup = BeautifulSoup(page.text, 'lxml')
-    looking_fornumber = soup.find(id="pagination").find_all('a')[-1].get('href')
-    number_ofpages = int(looking_fornumber[looking_fornumber.index('page=')+5:])
+    looking_for_number = soup.find(id="pagination").find_all('a')[-1].get('href')
+    number_of_pages = int(looking_for_number[looking_for_number.index('page=')+5:])
 
-    update_Ad_table_1(link, number_ofpages, listof_LinksInDB, listof_AllActiveUrlsInDB)
+    update_Ad_table_1(rss_link, number_of_pages, list_of_links_in_DB, list_of_all_active_urls_in_DB)
 
 
 
 # Part of update_Ad_table_0: takes a number of pages and existing URLS, as well as Active URLS
-#  and updates Ad_table with new Urls, also Updates existing Urls that expired. Returns a list
-#  of Urls that are not yet parsed for Job_table
+#  and updates Ad_table with new Urls, also Updates existing Urls that expired.
 
-def update_Ad_table_1(link, number_ofpages, listoflinksinDB, listofactiveUrls):
+def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrls):
 
     info_listofnewAds = []  # This is a container of all new URLs data, so that all data can be pushed to DB at once
-    list_ofnewUrls = []
     list_ofallUrls = []
 
     for number in range(number_ofpages+1):
-        pagelink = link[0:link.index('page=')+5] + str(number)
+        pagelink = rss_link[0:rss_link.index('page=')+5] + str(number)
 
         page = requests.get(pagelink)
         soup = BeautifulSoup(page.text, 'lxml')
-        onePage = soup.find_all('tr', itemtype="http://schema.org/JobPosting")
-        for JobAd in onePage:
-            Url = JobAd.find('a', class_="contentJobTitle").get('href')
-            list_ofallUrls.append(Url)
-            if Url not in listoflinksinDB:
+        one_page = soup.find_all('item')
 
-                templist = [] # Temporary container for data taken from URL that is latter appended to info_listof...
+        for JobAd in one_page:
+            url = JobAd.find('guid').get_text()
+            list_ofallUrls.append(url)
+            if url not in listoflinksinDB:
 
-                name = JobAd.find('a', class_="contentJobTitle").contents[0][1:]
-                views = JobAd.find('td', itemtype="http://schema.org/Place").contents[2][11:]
-                dateposted = JobAd.find(itemprop="datePosted").contents[0]
-                dateexpires = JobAd.find_all('p')[2].contents[0][22:]
+                temporary_list = [] # Temporary container for data taken from URL that is latter appended to info_listof...
 
-                templist.append(Url)
-                templist.append(dateposted)
-                templist.append(dateexpires)
-                templist.append('Active')
-                templist.append(str(datetime.date.today()))
-                templist.append(views)
-                templist.append(name)
-                info_listofnewAds.append(tuple(templist))
+                name = JobAd.find('job_function').get_text()
+                views = JobAd.find('td', itemtype="http://schema.org/Place").contents[2][12:]
+                date_posted = JobAd.find(itemprop="datePosted").get_text()
+                date_expires = JobAd.find_all('p')[2].contents[0][20:]
+                try:
+                    city = JobAd.find('category').get_text()
+                except:
+                    city = "Unspecified"
+                try:
+                    industry = JobAd.find('job_industry').get_text()
+                except:
+                    industry = "Unspecified"
+                try:
+                    employer_or_adagency = JobAd.find('employer').get_text()
+                except:
+                    employer_or_adagency = "Unspecified"
 
-                list_ofnewUrls.append(Url)
+
+                # Performs a parse with URL for content (keywords)
+                content_page = requests.get(url)
+                content_soup = BeautifulSoup(content_page.text, 'lxml')
+                content = content_soup.find('meta', {'name':'keywords'})['content']
+
+
+                temporary_list.append(url)
+                temporary_list.append(date_posted)
+                temporary_list.append(date_expires)
+                temporary_list.append('Active')
+                temporary_list.append(str(datetime.date.today()))
+                temporary_list.append(views)
+                temporary_list.append(name)
+                temporary_list.append(city)
+                temporary_list.append(industry)
+                temporary_list.append(content)
+                temporary_list.append(employer_or_adagency)
+
+                info_listofnewAds.append(tuple(temporary_list))
 
 
     # Check if All active URLs from DB are still active in new parse, if not update DB table with current date
     for activeUrl in listofactiveUrls:
         if activeUrl not in list_ofallUrls:
-            print(activeUrl)
-            conn.execute("UPDATE Ad_table set DateExpired = ? WHERE Url = ?", (str(datetime.date.today()), activeUrl))
+            print(activeUrl)  #čia šitas print šiai debuginimui :)
+            conn.execute("UPDATE Ad_table set DateExpired = ? WHERE url = ?", (str(datetime.date.today()), activeUrl))
             conn.commit()
 
     # Update DB with info of all new URLs using a list containing data of all new URLs
-    conn.executemany("INSERT INTO Ad_table VALUES (?,?,?,?,?,?,?)", info_listofnewAds)
+    conn.executemany("INSERT INTO Ad_table VALUES (?,?,?,?,?,?,?,?,?,?,?)", info_listofnewAds)
     conn.commit()
-    print(list_ofallUrls)
-    return list_ofnewUrls # A new function is supposed to take this and make a parse for Job information
+
+
 
 
 # Function called by update_Ad_table_0 to get existing URL list from DB
 def make_listoflinkinDB():
-    listofUrlsinDB = []
-    listofactiveUrls = []
-    retrieveUrls = conn.execute("SELECT * FROM Ad_table").fetchall()
-    for var in retrieveUrls:
-        listofUrlsinDB.append(var[0])
+    list_ofurlsindb = []
+    list_ofactiveurls = []
+    retrieve_urls = conn.execute("SELECT * FROM Ad_table").fetchall()
+    for var in retrieve_urls:
+        list_ofurlsindb.append(var[0])
         if var[3] == 'Active':
-            listofactiveUrls.append(var[0])
+            list_ofactiveurls.append(var[0])
 
-    return listofUrlsinDB, listofactiveUrls
+    return list_ofurlsindb, list_ofactiveurls
 
-listofpages = update_Ad_table_0(link)
+update_Ad_table_0(link)
 
 
 #####################################################
