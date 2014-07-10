5a537e1a84b2306aa5ec005b1c25836eef8aae21
CanisMajorisLT <frankaslt@yahoo.com>
2014-07-10 22:21:04 +0300

    Pakeitimai:
    Bendriniai:
    *Tai, kad updeitina skelbimą jokios įtakos duomenims neturi, rinkti duomenis apie tai įmanoma, bet ten nemažai pakoduot reikėtų, nes nėra per rss to duomens (kadangi ten datepublished išlieka toks pats) ir reikėtų eit kitu keliu, tai naudotu daug trafico.
    *Kaikurie Url jau turi po du skanavimus, nes pirmo metu nulužo intikas ir reikėjo paleist iš naujo kodą.
    
    Sqlite Database.db:
    ***Sukurta nauja matrica Main_Ad_Table nuo šiol duomenys bus renkami čia. Seni eina į šiūkšliudėžę, bet pofig.
    *DateAdded pervadintas į DateParsed: čia kiekvieno parsinimo metu įrašoma data + laikas per kablelį atskiriant nuo praeito karto. Pirma reikšmė yra kada buvo skelbimas įtrauktas, paskutinė reikšmė (jeigu skelbimas "Inacive") parodo kada skelbimas išimtas. Bet vis tiek palikau DateExpired stuleplį dėl visa ko.
    *Pridėtas naujas stulpelis IsActive: booleanas Active arba Inactive
    *Views dabar kiekviena kartą updeitinamas per kalbelį su nauja peržiūrų reikšme

--- Main.py
+++ Main.py
@@ -4,11 +4,10 @@ from bs4 import BeautifulSoup
 import sqlite3
 import datetime
 
-
 conn = sqlite3.connect("Database.db")
 
 # Takes furthest page link as argument and does magic
-def update_Ad_table_0():
+def update_Main_Ad_Table_0():
     link_for_getting_number_of_pages = 'http://www.cvonline.lt/darbo-skelbimai/visi?sort=tp_id&dir=desc&page=1000'
     rss_link = "http://www.cvonline.lt/darbo-skelbimai/visi?sort=tp_id&dir=desc&type=rss&page=0"
 
@@ -20,14 +19,14 @@ def update_Ad_table_0():
     looking_for_number = soup.find(id="pagination").find_all('a')[-1].get('href')
     number_of_pages = int(looking_for_number[looking_for_number.index('page=')+5:])
 
-    update_Ad_table_1(rss_link, number_of_pages, list_of_links_in_DB, list_of_all_active_urls_in_DB)
+    update_Main_Ad_Table_1(rss_link, number_of_pages, list_of_links_in_DB, list_of_all_active_urls_in_DB)
 
 
 
-# Part of update_Ad_table_0: takes a number of pages and existing URLS, as well as Active URLS
-#  and updates Ad_table with new Urls, also Updates existing Urls that expired.
+# Part of update_Main_Ad_Table_0: takes a number of pages and existing URLS, as well as Active URLS
+#  and updates Main_Ad_Table with new Urls, also Updates existing Urls that expired.
 
-def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrls):
+def update_Main_Ad_Table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrls):
 
     list_ofallUrls = []
 
@@ -43,6 +42,7 @@ def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrl
         for JobAd in one_page:
             url = JobAd.find('guid').get_text()
             list_ofallUrls.append(url)
+
             if url not in listoflinksinDB:
 
                 temporary_list = [] # Temporary container for data taken from URL that is latter appended to info_listof...
@@ -74,8 +74,9 @@ def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrl
                 temporary_list.append(url)
                 temporary_list.append(date_posted)
                 temporary_list.append(date_expires)
+                temporary_list.append('None')
+                temporary_list.append(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                 temporary_list.append('Active')
-                temporary_list.append(str(datetime.date.today()))
                 temporary_list.append(views)
                 temporary_list.append(name)
                 temporary_list.append(city)
@@ -85,17 +86,30 @@ def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrl
 
                 info_listofnewAds.append(tuple(temporary_list))
 
+            # Updates Views, DateParsed with values of a new parse
+            if url in listofactiveUrls:
+
+                views_updated = JobAd.find('td', itemtype="http://schema.org/Place").contents[2][12:]
+                old_values = conn.execute("SELECT Views, DateParsed, DatePublished FROM Main_Ad_Table WHERE Url = ?", (url,)).fetchone()
+                new_value_views = old_values[0] + ',' + views_updated
+                new_value_parsed = old_values[1] + ',' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                conn.execute("UPDATE Main_Ad_Table set Views = ?, DateParsed = ? WHERE url = ?",
+                             (new_value_views, new_value_parsed, url))
+
+
         print(number)
 
         # Update DB with info of all new URLs using a list containing data of all new URLs
-        conn.executemany("INSERT INTO Ad_table VALUES (?,?,?,?,?,?,?,?,?,?,?)", info_listofnewAds)
+        conn.executemany("INSERT INTO Main_Ad_Table VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", info_listofnewAds)
         conn.commit()
 
     # Check if All active URLs from DB are still active in new parse, if not update DB table with current date
     for activeUrl in listofactiveUrls:
         if activeUrl not in list_ofallUrls:
             print(activeUrl)  #čia šitas print šiai debuginimui :)
-            conn.execute("UPDATE Ad_table set DateExpired = ? WHERE url = ?", (str(datetime.date.today()), activeUrl))
+            conn.execute("UPDATE Main_Ad_Table set IsActive = ?, DateExpired = ? WHERE url = ?",
+                         ("Inactive", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), activeUrl)) #sukurt naują stalą
             conn.commit()
 
 
@@ -103,19 +117,19 @@ def update_Ad_table_1(rss_link, number_ofpages, listoflinksinDB, listofactiveUrl
 
 
 
-# Function called by update_Ad_table_0 to get existing URL list from DB
+# Function called by update_Main_Ad_Table_0 to get existing URL list from DB
 def make_listoflinkinDB():
     list_ofurlsindb = []
     list_ofactiveurls = []
-    retrieve_urls = conn.execute("SELECT * FROM Ad_table").fetchall()
+    retrieve_urls = conn.execute("SELECT * FROM Main_Ad_Table").fetchall()
     for var in retrieve_urls:
         list_ofurlsindb.append(var[0])
-        if var[3] == 'Active':
+        if var[5] == 'Active':
             list_ofactiveurls.append(var[0])
 
     return list_ofurlsindb, list_ofactiveurls
 
-update_Ad_table_0()
+update_Main_Ad_Table_0()
 
 
 #####################################################
